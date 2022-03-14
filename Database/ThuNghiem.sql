78e107ed0370c49a07916f17f65593560f712d38
loc <vanloc01699@gmail.com>
2022-03-14 20:40:22 +0700

    trang chu tinh

--- Database/ThuNghiem.sql
+++ Database/ThuNghiem.sql
@@ -1,18 +1,28 @@
 create database `ThuNghiem`;
 
 use `ThuNghiem`;
-drop table`thunghiem`;
-create table `test`(
+drop table`menu`;
+create table `menu`(
 	id int auto_increment,
     `name` varchar(225),
+    id_cha int,
     primary key(id)
 );
-insert into `ThuNghiem`.`test`
-(`id`, `name`) values
-('1', 'Nguyen van A'),
-('2', 'Nguyen van B'),
-('3', 'Nguyen van C'),
-('4', 'Nguyen van D');
+insert into `ThuNghiem`.`menu`
+(`id`, `name`,`id_cha`) values
+('2', 'TRANG CHU','0'),
+('3', 'CÁC MẪU XE','0'),
+('4', 'GIÁ XE HUYNDAI','0'),
+('5', 'MUA XE TRẢ GÓP','0'),
+('6', 'TIN TƯC','0'),
+('7', 'LIÊN HỆ','0'),
+('8', 'Huyndai Sedan','3'),
+('9', 'Huyndai Accent','3'),
+('10', 'Huyndai Santafe','3'),
+('11', 'Huyndai Tucson','3'),
+('12', 'Huyndai Sonata','3'),
+('13', 'Huyndai Elantra','3');
+
 
 
 select * from `test`;
--- Database/XeCuaToi.sql
+++ Database/XeCuaToi.sql
@@ -1,24 +1,17 @@
 create database `XeCuaToi`;
 
 use `XeCuaToi`;
-drop table `menu`
+drop table `menu`;
 create table `menu`(
 id_menu int auto_increment,
-trangchu varchar(225),
-mauxe varchar(225),
-giaxe varchar(225),
-tragop varchar(225),
-tintuc varchar(225),
-lienhe varchar(225),
+gia_tri varchar(225),
 primary key(id_menu)
 );
 insert into `Xecuatoi`.`menu`
-(`trangchu`,`mauxe`,`giaxe`,`tragop`,`tintuc`,`lienhe`) values
-('','huyndai_accent','','','',''),
-('','huyndai_santafe','','','',''),
-('','huyndai_tucson','','','',''),
-('','huyndai_sonata','','','',''),
-('','huyndai_elantra','','','','');
+(`id_menu`,`gia_tri`) values
+(12313,'Trang chu'),
+(123131,'Cac mau xe'),
+(1231311,'Gi xe huyndai');
 
 create table `mauxe`(
 id_mauxe int auto_increment,
@@ -29,6 +22,18 @@ huyndai_sonata varchar(225),
 huyndai_elantra varchar(225),
 primary key(id_mauxe)
 );
+drop table`ThuNghiem`;
+create table `ThuNghiem`(
+id int auto_increment,
+name varchar(225),
+primary key(id)
+);
+insert into`xecuatoi`.`ThuNghiem`
+(`id`,`name`) values
+(1,'ABC'),
+(2,'ABC'),
+(3,'ABC'),
+(4,'ABC');
 select * from `menu`
 
 
